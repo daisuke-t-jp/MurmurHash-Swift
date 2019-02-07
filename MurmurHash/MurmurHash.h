a44e80c9cb1833cc46e446968dbbbad0b1e420f6
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-07 20:56:18 +0900

    Add scheme for mac

--- MurmurHash/MurmurHash.h
+++ MurmurHash/MurmurHash.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 MurmurHash. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MurmurHash.
 FOUNDATION_EXPORT double MurmurHashVersionNumber;
