bdc42ac0ca7d28dfef3b0f4876f59b84f4b7e19d
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-28 19:41:09 +0900

    Update demo

--- demo/Common.swift
+++ demo/Common.swift
@@ -6,50 +6,45 @@
 //  Copyright © 2019 MurmurHashDemo. All rights reserved.
 //
 
-#if os(iOS) || os(tvOS)
-import UIKit
-#else
-import Cocoa
-#endif
+import Foundation
 
 import MurmurHash_Swift
 
 class Common {
 	
 	static func x86_32() {
-		print(String(format: "x86_32(\"Hello World!\") -> 0x%02x", MurmurHash3.x86_32("Hello World!")))
-		print(String(format: "x86_32(\"Hello World!\", seed: 0x7fffffff) -> 0x%02x", MurmurHash3.x86_32("Hello World!", seed: 0x7fffffff)))
+		print("x86_32(\"Hello World!\") -> \(MurmurHash3.x86_32("Hello World!"))")
+		print("x86_32(\"Hello World!\", seed: 0x7fffffff) -> \(MurmurHash3.x86_32("Hello World!", seed: 0x7fffffff))")
+		print("x86_32Hex(\"Hello World!\") -> 0x\(MurmurHash3.x86_32Hex("Hello World!"))")
+		print("x86_32Hex(\"Hello World!\", seed: 0x7fffffff) -> 0x\(MurmurHash3.x86_32Hex("Hello World!", seed: 0x7fffffff))")
 	}
 	
 	static func x86_32_file() {
 		let bundle = Bundle(for: Common.self)
 		let path = bundle.path(forResource: "alice29", ofType: "txt")!
 		let data = NSData(contentsOfFile: path)! as Data
-		
-		print(String(format: "x86_32(file) -> 0x%02x", MurmurHash3.x86_32(data)))
-		print(String(format: "x86_32(file, seed: 0x7fffffff) -> 0x%02x", MurmurHash3.x86_32(data, seed: 0x7fffffff)))
+
+		print("x86_32(file) -> \(MurmurHash3.x86_32(data))")
+		print("x86_32(file, seed: 0x7fffffff) -> \(MurmurHash3.x86_32(data, seed: 0x7fffffff))")
+		print("x86_32Hex(file) -> 0x\(MurmurHash3.x86_32Hex(data))")
+		print("x86_32Hex(file, seed: 0x7fffffff) -> 0x\(MurmurHash3.x86_32Hex(data, seed: 0x7fffffff))")
 	}
 	
 	
 	
 	static func x86_128() {
-		do {
-			let out = MurmurHash3.x86_128("Hello World!")
-			print("x86_128(\"Hello World!\") ->")
-			print(String(format: "    h1 0x%02x", out[0]))
-			print(String(format: "    h2 0x%02x", out[1]))
-			print(String(format: "    h3 0x%02x", out[2]))
-			print(String(format: "    h4 0x%02x", out[3]))
-		}
-		
-		do {
-			let out = MurmurHash3.x86_128("Hello World!", seed: 0x7fffffff)
-			print("x86_128(\"Hello World!\", seed: 0x7fffffff) ->")
-			print(String(format: "    h1 0x%02x", out[0]))
-			print(String(format: "    h2 0x%02x", out[1]))
-			print(String(format: "    h3 0x%02x", out[2]))
-			print(String(format: "    h4 0x%02x", out[3]))
-		}
+		print("x86_128(\"Hello World!\") ->" +
+			" \(MurmurHash3.x86_128("Hello World!")[0])" +
+			" \(MurmurHash3.x86_128("Hello World!")[1])" +
+			" \(MurmurHash3.x86_128("Hello World!")[2])" +
+			" \(MurmurHash3.x86_128("Hello World!")[3])")
+		print("x86_128(\"Hello World!\", seed: 0x7fffffff) ->" +
+			" \(MurmurHash3.x86_128("Hello World!", seed: 0x7fffffff)[0])" +
+			" \(MurmurHash3.x86_128("Hello World!", seed: 0x7fffffff)[1])" +
+			" \(MurmurHash3.x86_128("Hello World!", seed: 0x7fffffff)[2])" +
+			" \(MurmurHash3.x86_128("Hello World!", seed: 0x7fffffff)[3])")
+		print("x86_128Hex(\"Hello World!\") -> 0x\(MurmurHash3.x86_128Hex("Hello World!"))")
+		print("x86_128Hex(\"Hello World!\", seed: 0x7fffffff) -> 0x\(MurmurHash3.x86_128Hex("Hello World!", seed: 0x7fffffff))")
 	}
 	
 	static func x86_128_file() {
@@ -57,41 +52,31 @@ class Common {
 		let path = bundle.path(forResource: "alice29", ofType: "txt")!
 		let data = NSData(contentsOfFile: path)! as Data
 		
-		do {
-			let out = MurmurHash3.x86_128(data)
-			print("x86_128(file) ->")
-			print(String(format: "    h1 0x%02x", out[0]))
-			print(String(format: "    h2 0x%02x", out[1]))
-			print(String(format: "    h3 0x%02x", out[2]))
-			print(String(format: "    h4 0x%02x", out[3]))
-		}
-		
-		do {
-			let out = MurmurHash3.x86_128(data, seed: 0x7fffffff)
-			print("x86_128(file, seed: 0x7fffffff) ->")
-			print(String(format: "    h1 0x%02x", out[0]))
-			print(String(format: "    h2 0x%02x", out[1]))
-			print(String(format: "    h3 0x%02x", out[2]))
-			print(String(format: "    h4 0x%02x", out[3]))
-		}
+		print("x86_128(\"file\") ->" +
+			" \(MurmurHash3.x86_128(data)[0])" +
+			" \(MurmurHash3.x86_128(data)[1])" +
+			" \(MurmurHash3.x86_128(data)[2])" +
+			" \(MurmurHash3.x86_128(data)[3])")
+		print("x86_128(\"file!\", seed: 0x7fffffff) ->" +
+			" \(MurmurHash3.x86_128(data, seed: 0x7fffffff)[0])" +
+			" \(MurmurHash3.x86_128(data, seed: 0x7fffffff)[1])" +
+			" \(MurmurHash3.x86_128(data, seed: 0x7fffffff)[2])" +
+			" \(MurmurHash3.x86_128(data, seed: 0x7fffffff)[3])")
+		print("x86_128Hex(\"file\") -> 0x\(MurmurHash3.x86_128Hex(data))")
+		print("x86_128Hex(\"file\", seed: 0x7fffffff) -> 0x\(MurmurHash3.x86_128Hex(data, seed: 0x7fffffff))")
 	}
 	
 	
 	
 	static func x64_128() {
-		do {
-			let out = MurmurHash3.x64_128("Hello World!")
-			print("x64_128(\"Hello World!\") ->")
-			print(String(format: "    h1 0x%02lx", out[0]))
-			print(String(format: "    h2 0x%02lx", out[1]))
-		}
-		
-		do {
-			let out = MurmurHash3.x64_128("Hello World!", seed: 0x7fffffff)
-			print("x64_128(\"Hello World!\", seed: 0x7fffffff) ->")
-			print(String(format: "    h1 0x%02lx", out[0]))
-			print(String(format: "    h2 0x%02lx", out[1]))
-		}
+		print("x64_128(\"Hello World!\") ->" +
+			" \(MurmurHash3.x64_128("Hello World!")[0])" +
+			" \(MurmurHash3.x64_128("Hello World!")[1])")
+		print("x64_128(\"Hello World!\", seed: 0x7fffffff) ->" +
+			" \(MurmurHash3.x64_128("Hello World!", seed: 0x7fffffff)[0])" +
+			" \(MurmurHash3.x64_128("Hello World!", seed: 0x7fffffff)[1])")
+		print("x64_128Hex(\"Hello World!\") -> 0x\(MurmurHash3.x64_128Hex("Hello World!"))")
+		print("x64_128Hex(\"Hello World!\", seed: 0x7fffffff) -> 0x\(MurmurHash3.x64_128Hex("Hello World!", seed: 0x7fffffff))")
 	}
 	
 	static func x64_128_file() {
@@ -99,19 +84,14 @@ class Common {
 		let path = bundle.path(forResource: "alice29", ofType: "txt")!
 		let data = NSData(contentsOfFile: path)! as Data
 
-		do {
-			let out = MurmurHash3.x64_128(data)
-			print("x64_128(file) ->")
-			print(String(format: "    h1 0x%02lx", out[0]))
-			print(String(format: "    h2 0x%02lx", out[1]))
-		}
-		
-		do {
-			let out = MurmurHash3.x64_128(data, seed: 0x7fffffff)
-			print("x64_128(file, seed: 0x7fffffff) ->")
-			print(String(format: "    h1 0x%02lx", out[0]))
-			print(String(format: "    h2 0x%02lx", out[1]))
-		}
+		print("x64_128(\"file\") ->" +
+			" \(MurmurHash3.x64_128(data)[0])" +
+			" \(MurmurHash3.x64_128(data)[1])")
+		print("x64_128(\"file!\", seed: 0x7fffffff) ->" +
+			" \(MurmurHash3.x64_128(data, seed: 0x7fffffff)[0])" +
+			" \(MurmurHash3.x64_128(data, seed: 0x7fffffff)[1])")
+		print("x64_128Hex(\"file\") -> 0x\(MurmurHash3.x64_128Hex(data))")
+		print("x64_128Hex(\"file\", seed: 0x7fffffff) -> 0x\(MurmurHash3.x64_128Hex(data, seed: 0x7fffffff))")
 	}
 
 }
